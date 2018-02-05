fe6a023af5d6703fe29841d8c2abf8782d3f2ddc
Leo B <leo.benkel@gmail.com>
2018-02-04 16:30:41 -0800

    Fix first render of the game

--- js/main-demo.js
+++ js/main-demo.js
@@ -2,16 +2,19 @@ define([
         'jquery',
         'lodash',
         './app/game.js',
-    ], function($, _, Game) {
-        var game = new Game(
+        './utility/resize-canvas.js',
+    ], function($, _, Game, resizeCanvas) {
+        window.game = new Game(
             './game-files-examples/test-game-1.json',
             document.getElementById('canvas'),
             null
         );
 
         $('.CanvasArea').removeClass('is-hidden');
-        game.start()
+        window.game.start()
         .then(() => {
+            resizeCanvas();
+            window.game.rerender();
             console.log('App started.');
         });
 });
