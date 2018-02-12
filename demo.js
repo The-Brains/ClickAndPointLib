90450ae9e408bdeae6a2af335cda45a0f1d8bf02
Leo B <leo.benkel@gmail.com>
2018-02-11 19:30:18 -0800

    Add beginning of response system

--- demo.js
+++ demo.js
@@ -31,7 +31,6 @@ define(['js/utility/find-get-param.js'], function(findGetParameter) {
     // Start loading the main app file. Put all of
     // your application logic in there.
     requirejs([
-        // 'js/utility/resize-canvas.js',
         'js/main-demo.js',
     ]);
 });
